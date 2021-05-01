22eda406d64fab8607d8150f28e181c27fc4699b
Aymen Ayed <aymen.ayed@esprit.tn>
2021-05-01 20:08:06 +0200

    test covrage achive 73%

--- src/app/table/table.component.spec.ts
+++ src/app/table/table.component.spec.ts
@@ -1,27 +1,48 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, inject, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Posts } from '../_models/posts';
+import { PostsService } from '../_services/posts.service';
 
 import { TableComponent } from './table.component';
 
 describe('TableComponent', () => {
   let component: TableComponent;
   let fixture: ComponentFixture<TableComponent>;
+  let postsService: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      declarations: [ TableComponent ]
+      declarations: [ TableComponent ],
+      providers: [
+        PostsService
+      ],
+      
     })
     .compileComponents();
     
   });
 
-  beforeEach(() => {
+  beforeEach(inject([PostsService],(s: any)=> {
+    postsService=s;
     fixture = TestBed.createComponent(TableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
+  it("should call showPosts and return list of posts",fakeAsync(() => {
+    const response: Posts[] = [];
+  
+    spyOn(postsService, 'getPostByday').and.returnValue(of(response))
+  
+    component.showPosts();
+  
+    fixture.detectChanges();
+  
+    expect(component.posts).toEqual(response);
+  }));
+ 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
